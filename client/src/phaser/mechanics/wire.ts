bf4f92f272b5f891bb18658f3c8f96667c608514
bnszky <michal.banaszkiewicz.pvt@gmail.com>
2026-03-16 12:46:20 +0100

    fix: fix rotation angle and semicolons

--- client/src/phaser/mechanics/wire.ts
+++ client/src/phaser/mechanics/wire.ts
@@ -1,40 +1,39 @@
-
 import { Mechanic } from "./mechanic";
+
 const WIRE_FRAME = 24;
 
 export class Wire extends Mechanic {
-    public wireId: string;
-    private direction: "up" | "down" | "left" | "right";
+  public wireId: string;
+  private direction: "up" | "down" | "left" | "right";
 
-    constructor(
-        scene: Phaser.Scene,
-        x: number,
-        y: number,
-        wireId: string,
-        direction: "up" | "down" | "left" | "right";
-    ) {
-        super(scene, x, y, WIRE_FRAME);
-        this.wireId = wireId,
-            this.direction = direction;
+  constructor(
+    scene: Phaser.Scene,
+    x: number,
+    y: number,
+    wireId: string,
+    direction: "up" | "down" | "left" | "right",
+  ) {
+    super(scene, x, y, WIRE_FRAME);
+    this.wireId = wireId;
+    this.direction = direction;
 
-        switch (this.direction) {
-            case "up":
-                this.setAngle(0);
-                break;
-            case "right":
-                this.setAngle(90);
-                break;
-            case "down":
-                this.setAngle(180);
-                break;
-            case "left":
-                this.setAngle(270);
-                break;
-        }
-
+    switch (this.direction) {
+      case "up":
+        this.setAngle(270);
+        break;
+      case "right":
+        this.setAngle(0);
+        break;
+      case "down":
+        this.setAngle(90);
+        break;
+      case "left":
+        this.setAngle(180);
+        break;
     }
+  }
 
-    public get id(): string {
-        return this.wireId;
-    }
+  public get id(): string {
+    return this.wireId;
+  }
 }
