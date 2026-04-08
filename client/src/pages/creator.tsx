4441bc05e89ce86acd750b8db1e46b073b374666
alsjkdflkw <289861@student.pwr.edu.pl>
2026-04-08 21:58:20 +0200

    feat: reorganize creator items select by layers

--- client/src/pages/creator.tsx
+++ client/src/pages/creator.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
 import { CreatorBoard } from "../components/creator/creator-board/creator-board";
 import { CreatorControl } from "../components/creator/creator-control/creator-control";
@@ -8,6 +8,7 @@ import { CreatorName } from "../components/creator/creator-name/creator-name";
 export function Creator() {
   const [levelName, setLevelName] = useState<string>();
   const [dims, setDims] = useState<[number, number]>([7, 8]);
+  const [activeBlock, setActiveBlock] = useState<{ key: string; frame: number; label: string } | null>(null);
 
   return (
     <div className="mx-4 mt-[2dvh] mb-[2dvh] flex w-full flex-col gap-[2dvh] text-center">
@@ -21,10 +22,10 @@ export function Creator() {
       </div>
       <div className="flex h-[80dvh] w-full items-stretch gap-4">
         <div className="w-2/5">
-          <CreatorItemsSelect />
+          <CreatorItemsSelect activeBlock={activeBlock} setActiveBlock={setActiveBlock} />
         </div>
         <div className="w-3/5">
-          <CreatorBoard dims={dims} />
+          <CreatorBoard dims={dims} activeBlock={activeBlock} />
         </div>
       </div>
     </div>
