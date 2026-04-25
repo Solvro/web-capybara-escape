9dae2467f0b6cc739d9b292ea82b62a5d5035f79
24bartixx <jestgitbartix@gmail.com>
2026-04-25 13:42:21 +0200

    chore: remove unused imports

--- client/src/components/creator/creator-board/creator-board.tsx
+++ client/src/components/creator/creator-board/creator-board.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { LAYER_ORDER } from "../../../constants/layer-items";
 import type { LayerItem } from "../../../constants/layer-items";
