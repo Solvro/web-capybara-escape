05930663326a95c25a8821b57745fa0d5805548d
Harukume <harusume1001@gmail.com>
2026-04-26 10:20:40 +0200

    chore: format code with prettier

--- server/src/app.config.ts
+++ server/src/app.config.ts
@@ -13,7 +13,7 @@ export default config({
      * Define your room handlers:
      */
     gameServer.define("game_room", GameRoom);
-     // .filterBy(['isPrivate']);
+    // .filterBy(['isPrivate']);
   },
 
   initializeExpress: (app) => {
