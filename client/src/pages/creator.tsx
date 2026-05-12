8e641b60ed838cd0dfd34e85a2f2d0616ef78d4d
Oliwier K <o.krawczyk06@gmail.com>
2026-05-12 17:44:37 +0200

    feat: add the code

--- client/src/pages/creator.tsx
+++ client/src/pages/creator.tsx
@@ -29,7 +29,13 @@ export function Creator() {
     ASSETS.WIRE,
     ASSETS.BUTTON_RELEASED,
   ];
-  const entities = [ASSETS.CRATE, ASSETS.LASER_GUN];
+  const entities = [
+    ASSETS.CRATE, 
+    ASSETS.LASER_GUN, 
+    ASSETS.CAPYBARA,
+    ASSETS.SOL,
+    ASSETS.VRON
+  ];
   const wallDecoys = [ASSETS.DOOR_CLOSED];
 
   const [tileIndices, setTileIndices] = useState<(number | null)[][]>(() =>
